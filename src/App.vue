9787f03b65389a00434b0f9b6da2e923065bb47a
Joddie <Jers.033@gmail.com>
2024-11-03 18:07:53 -0600

    finish

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <select v-model="$i18n.locale">
+    <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
+      {{ locale }}
+  </select>
+  <h1> {{ $t("messages.hello",{name,surname})}} </h1>
+  <p> {{ $t("messages.content",{description}) }} </p>
+  <p> {{ $t("messages.products",{count:1})}}</p>
+  <p> {{ $t("messages.comments",5) }}</p>
+</template>
+  
+<script lang="ts" setup>
+  import {ref} from 'vue';
+  const name = ref('Manuel');
+  const description = ref('Description');
+  const surname = ref('Garcia');
+</script>
+  
+<style scoped>
+</style>
+  
---
+++ src/lang/messages.ts
@@ -0,0 +1,18 @@
+export const messages = {
+    en:{
+        messages: {
+            hello: "Hello {name} {surname}",
+            content:"This is a simple Vue 3 app. {description}",
+            products: "no products | one product | {count} products",
+            comments: "no comments | one comment | {count} comments"
+        }
+    },
+    es:{
+        messages: {
+            hello: "Hola {name} {surname}",
+            content:"Esta es una aplicación simple de Vue 3. {description}",
+            products: "no hay productos | un producto | {count} productos",
+            comments: "no hay comentarios | un comentario | {count} comentarios"
+        }
+    },
+}
